a443eb2529a543eecd478aebf135981083313c5c
林宥丞 <a26913700@gmail.com>
2018-11-27 19:12:22 +0800

    (補) 3個路由對應到3個Controller

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('home');
-});
-Route::get('home/{name?}', ['as' => 'home.index', function() {
-    return view('home');
-} ] );
-Route::get('about/{name?}', ['as' => 'about.index', function() {
-    return view('about');
-} ] );
-Route::get('news/{name?}', ['as' => 'news.index', function() {
-    return view('news');
-} ] );
+Route::get('/', ['as'=>'home.index','uses'=>'HomeController@index']);
+Route::get('/about', ['as'=>'about.index','uses'=>'AboutController@index']);
+Route::get('/news', ['as'=>'news.index','uses'=>'NewsController@index']);
